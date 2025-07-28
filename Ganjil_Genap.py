509c902e90f483ecaf65bb9ae5c972ffff9b327e
Muhammad Rizki Wisnu Hidayat <muhammadrizkiwisnuhidayat@gmail.com>
2025-07-28 14:02:45 +0700

    Add files via upload

---
+++ Ganjil_Genap.py
@@ -0,0 +1,7 @@
+# Program memeriksa ganjil genap
+angka = int(input("Masukan sebuah angka: "))
+
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap.")
+else:
+    print(f"{angka} adalah bilangan ganjil.")
