b8a4ad07246f83119d4dd9761bc1ef28d5782d32
Muhammad Rizki Wisnu Hidayat <muhammadrizkiwisnuhidayat@gmail.com>
2025-08-04 13:19:39 +0700

    Add files via upload

---
+++ Akar_Kuadrat.py
@@ -0,0 +1,6 @@
+# Menghitung akar kuadrat
+import math
+
+angka = float(input("Masukkan sebuah angka: "))
+akar_kuadrat = math.sqrt(angka)
+print(f"Akar kuadrat dari {angka} adalah {akar_kuadrat:.3f}")
