e3ba04785bfefcac4c7c3fa5dabd9c1e5827d40a
Muhammad Rizki Wisnu Hidayat <muhammadrizkiwisnuhidayat@gmail.com>
2025-08-04 13:31:03 +0700

    Add files via upload

---
+++ Luas_Segitiga.py
@@ -0,0 +1,6 @@
+# Program menghitung Luas segitiga
+alas = float(input("Tulis alas segitiga: "))
+tinggi = float(input("Tulis tinggi segitiga: "))
+
+luas = (alas * tinggi) / 2
+print(f"Luas segitiga adalah {luas:.2f}")
