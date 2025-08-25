9ed3223a0b0e9a585fe99991de600ba0350c75bb
Muhammad Rizki Wisnu Hidayat <muhammadrizkiwisnuhidayat@gmail.com>
2025-08-25 14:02:00 +0700

    Add files via upload

---
+++ Celcius_Fahrenheit.py
@@ -0,0 +1,4 @@
+# Program konversi Celcius ke Fahrenheit
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+fahrenheit = (celcius * 9/5) + 32
+print(f"{celcius} derajat Celcius sama dengan {fahrenheit:.2f} derajat Fahrenheit")
---
+++ Fahrenheit_Celcius.py
@@ -0,0 +1,4 @@
+# Program konversi Fahrenheit ke Celcius
+fahrenheit = float(input("Masukkan suhu dalam Fahrenheit: "))
+celcius = (fahrenheit - 32) * 5/9
+print(f"{fahrenheit} derajat Fahrenheit sama dengan {celcius:.2f} derajat Celcius")
